e4f391ff99e4f029793c34e8d9d96bad0adc6a70
Tetyana Murzina <tetyanamur@gmail.com>
2022-08-02 22:17:36 -0500

    initial commit

---
+++ EncapsulationExercise/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncapsulationExercise
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+        }
+        private double balance = 0;
+        public double Balance
+            { get { return balance; } set { balance = value; } }
+        public void  Deposit (double amount)
+        {
+            balance = amount;
+        }
+        public double Getbalance()
+            {
+            return $"${balance}";
+        }
+    }
+}
---
+++ EncapsulationExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EncapsulationExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount();
+            Console.WriteLine("Deposit?");
+            var amountToDep = double.Parse(Console.ReadLine());
+            account.Deposit(amountToDep);
+            Console.WriteLine($"Thank you. Balance is now {account.Getbalance()}");
+        }
+    }
+}
